7402b505c0fdb974cf90547d6d7302258c2dbb61
Ann Johansson <ann.johansson.work@gmail.com>
2026-05-04 23:51:27 +0200

    Made initial Clean Architecture skeleton with Book and ReadingSession entities and a IBookRepository

---
+++ ReadingNook.Domain/Interfaces/IBookRepository.cs
@@ -0,0 +1,16 @@
+﻿using ReadingNook.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ReadingNook.Domain.Interfaces
+{
+    public interface IBookRepository
+    {
+        Task<IEnumerable<Book>> GetAllAsync();
+        Task<Book?> GetByIdAsync(int id);
+        Task AddAsync(Book book);
+        Task UpdateAsync(Book book);
+        Task DeleteAsync(int id);
+    }
+}
